f3c4a84436bf5a96c84367eeebc32243d30cbcda
Boris Kuznetsov <achempion@gmail.com>
2015-03-31 00:37:34 +0300

    ember

---
+++ app/assets/javascripts/ember/tasks.js
@@ -0,0 +1,18 @@
+window.Tasks = Ember.Application.create({});
+
+Tasks.Router.map(function() {
+  this.resource('tasks', { path: '/' });
+});
+
+Tasks.Task = DS.Model.extend({
+    title: DS.attr('string'),
+    is_done: DS.attr('boolean')
+});
+
+Tasks.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Tasks.TasksRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.find('task');
+    }
+});
